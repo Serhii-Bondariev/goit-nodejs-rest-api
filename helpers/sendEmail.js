fa47f1ec690eb5f85e90bc86476d150d4b6c583a
Serhii <xx.code4u@gmail.com>
2024-04-04 21:43:51 +0300

    saaaxacdsdcfs

---
+++ helpers/sendEmail.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { UKRNET_MAIL_FROM, UKRNET_MAIL_PASSWORD } = process.env;
+
+const transport = nodemailer.createTransport({
+  host: "smtp.ukr.net",
+  port: 465,
+  auth: {
+    user: UKRNET_MAIL_FROM,
+    pass: UKRNET_MAIL_PASSWORD,
+  },
+});
+
+function sendEmail(message) {
+  return transport.sendMail(message);
+}
+
+export default sendEmail;

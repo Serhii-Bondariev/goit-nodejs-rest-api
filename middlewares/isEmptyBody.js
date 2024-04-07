aab26455b1116bbcd9d990bfe5203e180008b5f0
Serhii <xx.code4u@gmail.com>
2024-04-07 12:37:18 +0300

    Update isEmptyBody.js

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -1,9 +1,31 @@
-import { HttpError } from "../helpers/index.js";
+// import { HttpError } from "../helpers/index.js";
+
+// const isEmptyBody = (req, res, next) => {
+//   if (!Object.keys(req.body).length) {
+//     return next(HttpError(400, "missing fields"));
+//   }
+//   next();
+// };
+
+// export default isEmptyBody;
+
+import Joi from "joi";
 
 const isEmptyBody = (req, res, next) => {
-  if (!Object.keys(req.body).length) {
-    return next(HttpError(400, "missing fields"));
+  const schema = Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    const errorMessage = error.details
+      .map((detail) => detail.message)
+      .join("; ");
+    return res.status(400).json({ message: errorMessage });
   }
+
   next();
 };
 
